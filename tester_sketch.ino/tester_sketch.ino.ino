f7c88d884484e68e4cc614c46eb318b51e4c9695
Greg White <gregcwbusiness@gmail.com>
2026-09-21 10:43:05 +0100

    Added MOSFET tester sketch and modified speaker pin in main .ino

---
+++ tester_sketch.ino/tester_sketch.ino.ino
@@ -0,0 +1,17 @@
+const int speakerPowerPin = 10;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(speakerPowerPin, OUTPUT);
+  digitalWrite(speakerPowerPin, LOW); // start off, matches your pull-down safety
+}
+
+void loop() {
+  digitalWrite(speakerPowerPin, HIGH);
+  Serial.println("GPIO10: HIGH (LED/amp should be ON)");
+  delay(4000);
+
+  digitalWrite(speakerPowerPin, LOW);
+  Serial.println("GPIO10: LOW (LED/amp should be OFF)");
+  delay(4000);
+}
